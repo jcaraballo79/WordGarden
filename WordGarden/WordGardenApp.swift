d66d39574dc4330e45bde1e40d934eaad522e76a
Jorge J. Caraballo <jcaraballo79@gmail.com>
2026-01-14 18:00:02 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Jorge Caraballo on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
